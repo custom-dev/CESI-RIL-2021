0006557a1adef9d7d70584b168bf6278e0422478
François DORIN <github@custom>
2022-01-27 13:55:31 +0100

    Refactoring

--- CESI.CIL/VDM/VieDeMerde.cs
+++ CESI.CIL/VDM/VieDeMerde.cs
@@ -14,15 +14,26 @@ namespace CESI.CLI.VDM
 		public static VieDeMerde Parse(HtmlNode node)
 		{
 			VieDeMerde vdm = new VieDeMerde();
-			HtmlNode titleNode = node.SelectSingleNode(".//h2/a");
-			HtmlNode contentNode = node.SelectSingleNode("./article/a");
 
-			vdm.Title = titleNode.InnerText;
-			vdm.Content = HttpUtility.HtmlDecode(contentNode.InnerText).Trim();
+			vdm.ExtractTitle(node);
+			vdm.ExtractContent(node);
+
 			return vdm;
 		}
 
 		public string Title { get; private set; }
 		public string Content { get;private set; }
+
+		private void ExtractTitle(HtmlNode node)
+		{
+			HtmlNode titleNode = node.SelectSingleNode(".//h2/a");
+			this.Title = titleNode.InnerText;
+		}
+
+		private void ExtractContent(HtmlNode node)
+		{
+			HtmlNode contentNode = node.SelectSingleNode("./article/a");
+			this.Content = HttpUtility.HtmlDecode(contentNode.InnerText).Trim();
+		}
 	}
 }
